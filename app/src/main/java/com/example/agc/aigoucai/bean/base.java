e6d4462c55ce5480fe76e1260ad716143daab79d
DESKTOP-VFHMO2M\o <jason19990099@gmail.com>
2018-06-28 16:41:23 +0800

    修改设置、

--- app/src/main/java/com/example/agc/aigoucai/bean/base.java
+++ app/src/main/java/com/example/agc/aigoucai/bean/base.java
@@ -5,7 +5,7 @@ public class base {
     public static  final String appid="android101";
     //分享链接 500  https://www.500app.me/app.html
     //爱购彩 https://www.agcapp.me/app.html
-    public static  final String share_url="https://www.agcapp.me/app.html";
+    public static  final String share_url="https://www.500app.me/app.html";
 
 
     //注意要修改配置文件的推送設置
