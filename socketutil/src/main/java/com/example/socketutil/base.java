fe4039c9cef0371979a0dd8b75828166e0a9378d
DESKTOP-VFHMO2M\o <jason19990099@gmail.com>
2018-07-10 18:40:41 +0800

    继承、

---
+++ socketutil/src/main/java/com/example/socketutil/base.java
@@ -0,0 +1,27 @@
+package com.example.socketutil;
+
+public class base {
+
+    public static final String appid = "android101";
+    //分享链接 500  https://www.500app.me/app.html
+    //爱购彩 https://www.agcapp.me/app.html
+//
+//
+//    K7娱乐城
+//    https://www.k7app.me/app.html
+//
+//
+//    TT娱乐城
+//    https://www.ttapp.me/app.html
+//
+//    掌中彩
+//    https://www.appkings.me/zzc.html
+//
+//            678娱乐城
+//    https://www.appkings.me/678.html
+//    新葡京
+//    https://www.appkings.me/xpj.html
+    public static final String share_url = "https://www.agcapp.me/app.html";
+    //注意要修改配置文件的推送設置
+
+}
