c847643a6136288a066e4d777bc65b540b584b00
DESKTOP-VFHMO2M\o <jason19990099@gmail.com>
2018-07-16 16:40:34 +0800

    替换ip

--- app/src/main/java/com/example/agc/aigoucai/activity/SplashActivity.java
+++ app/src/main/java/com/example/agc/aigoucai/activity/SplashActivity.java
@@ -20,9 +20,8 @@ public class SplashActivity extends AppCompatActivity {
 
         List<String> ip_array = new ArrayList<>();
         ip_array.clear();
-        ip_array.add("39.106.217.117");
-        ip_array.add("222.186.42.23");
-        ip_array.add("103.17.116.117");
+        ip_array.add("112.74.175.185");
+        ip_array.add("39.106.148.87");
         //ip和端口号传进去
         SocketUtil socketUtil=new SocketUtil(ip_array,1985);
         //调取方法开始连接
