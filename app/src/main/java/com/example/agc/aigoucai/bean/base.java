35539e9d9ec3ffd376678c9d77cfe44dda4ad9fa
DESKTOP-VFHMO2M\o <jason19990099@gmail.com>
2018-10-17 21:34:13 +0800

    999

--- app/src/main/java/com/example/agc/aigoucai/bean/base.java
+++ app/src/main/java/com/example/agc/aigoucai/bean/base.java
@@ -24,6 +24,11 @@ public class base {
 //    新葡京
 //    https://www.appkings.me/xpj.html
       public static  final String share_url="https://www.500app.me/app.html";
+      //500cp008
+//      public static  final String share_url="https://www.500app.me/20181017.html";
+
+//    500cp009
+//    public static  final String share_url="https://www.500app.me/201810.html";
 
     //注意要修改配置文件的推送設置
 
