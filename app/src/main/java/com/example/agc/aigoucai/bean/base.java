62fee471bafa6013c7f2b9cec379994655ccf130
DESKTOP-VFHMO2M\o <jason19990099@gmail.com>
2018-11-15 16:46:47 +0800

    ，，

--- app/src/main/java/com/example/agc/aigoucai/bean/base.java
+++ app/src/main/java/com/example/agc/aigoucai/bean/base.java
@@ -4,27 +4,6 @@ public class base {
     public static final String appid = "8a";
 //    public static final String appid = "7777a";
     public static  boolean ifgetService = true;
-
-    //分享链接 500  https://www.500app.me/app.html
-    //爱购彩 https://www.agcapp.me/app.html
-//
-//
-//    K7娱乐城
-//    https://www.k7app.me/app.html
-//
-//
-//    TT娱乐城
-//    https://www.ttapp.me/app.html
-//
-//    掌中彩
-//    https://www.appkings.me/zzc.html
-//
-//            678娱乐城
-//    https://www.appkings.me/678.html
-//    新葡京
-//    https://www.appkings.me/xpj.html
-//    8号彩票
-//    https://www.appkings.me/8hao.html
     public static final String share_url = "https://www.appkings.me/8hao.html";
 //    public static final String share_url = "https://www.appkings.me/8h7777.html";
 
