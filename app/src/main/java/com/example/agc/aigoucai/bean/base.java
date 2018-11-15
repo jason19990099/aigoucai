d72142e3c9f1ca0f7b790493e9c75d1e7c36a567
DESKTOP-VFHMO2M\o <jason19990099@gmail.com>
2018-11-15 16:49:49 +0800

    删除无用代码、

--- app/src/main/java/com/example/agc/aigoucai/bean/base.java
+++ app/src/main/java/com/example/agc/aigoucai/bean/base.java
@@ -4,7 +4,6 @@ public class base {
 
     public static  String appid = "android909";
     public static  boolean ifgetService = true;
-//    https://www.appkings.me/xpj.html
     public static final String share_url = "https://www.appkings.me/zxc.html";
     //注意要修改配置文件的推送設置
 
