7835276e7bc2070c1881823c957bae8775d6d571
DESKTOP-VFHMO2M\o <jason19990099@gmail.com>
2018-11-15 16:33:39 +0800

    啊啊

--- app/src/main/java/com/example/agc/aigoucai/bean/base.java
+++ app/src/main/java/com/example/agc/aigoucai/bean/base.java
@@ -3,25 +3,6 @@ package com.example.agc.aigoucai.bean;
 public class base {
     public static final String appid = "android904";
     public static  boolean ifgetService = true;
-
-    //分享链接 500  https://www.500app.me/app.html
-    //爱购彩 https://www.agcapp.me/app.html
-//
-//
-//    K7娱乐城
-//    https://www.k7app.me/app.html
-//
-//
-//    TT娱乐城
-//    https://www.ttapp.me/app.html
-//
-//    掌中彩
-//    https://www.appkings.me/zzc.html
-//
-//            678娱乐城
-//    https://www.appkings.me/678.html
-//    新葡京
-//    https://www.appkings.me/xpj.html
     public static final String share_url = "https://www.appkings.me/xpj.html";
     //注意要修改配置文件的推送設置
 
