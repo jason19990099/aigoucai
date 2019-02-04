0d1e6f87509c05a8d4d54d7ac58276a312e8da25
DESKTOP-VFHMO2M\o <jason19990099@gmail.com>
2019-02-04 13:16:17 +0800

    1111

--- app/src/main/java/com/example/agc/aigoucai/activity/SplashActivity.java
+++ app/src/main/java/com/example/agc/aigoucai/activity/SplashActivity.java
@@ -1,10 +1,14 @@
 package com.example.agc.aigoucai.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
-
+import android.util.Log;
 
 import com.example.agc.aigoucai.R;
 import com.example.agc.aigoucai.util.Apputil;
@@ -14,7 +18,14 @@ import com.example.agc.aigoucai.util.SystemUtil;
 import java.util.ArrayList;
 import java.util.List;
 
-
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 
 /**
@@ -25,44 +36,55 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
         SystemUtil.setfullScreen(SplashActivity.this);
 
-        final List<String> ip_array = new ArrayList<>();
-        ip_array.clear();
-
-
-         new Thread(new Runnable() {
-             @Override
-             public void run() {
-                 String[] strings= Apputil.parseHostGetIPAddress("bobo.shyqyl.com");
-                 if (null==strings){
-                     return;
-                 }
-                 for (int i=0;i<strings.length;i++){
-                     LogUtil.e("========strings[i]===="+strings[i]);
-                     ip_array.add(strings[i]);
-                 }
-                 runOnUiThread(new Runnable() {
-                     @Override
-                     public void run() {
-                         //ip和端口号传进去
-                      SocketUtil socketUtil=new SocketUtil(ip_array,1985,SplashActivity.this);
-                     //调取方法开始连接
-                      socketUtil.getSocketConection();
-                     }
-                 });
-
-
-             }
-         }).start();
-
-        new Handler().postDelayed(new Runnable() {
+        Observable<List<String>> oble = Observable.create(new ObservableOnSubscribe<List<String>>() {
             @Override
-            public void run() {
+            public void subscribe(@NonNull ObservableEmitter<List<String>> e) throws Exception {
+
+                List<String> ip_array = new ArrayList<>();
+                ip_array.clear();
+                String[] strings= Apputil.parseHostGetIPAddress("bobo.shyqyl.com");
+                if (null==strings){
+                    return;
+                }
+                int size=strings.length;
+                for (int i=0;i<size;i++){
+                    ip_array.add(strings[i]);
+                }
+                e.onNext(ip_array);
+                SystemClock.sleep(1500);
+                e.onComplete();
+
+            }
+        });
+
+        Observer<List<String>> oser = new Observer<List<String>>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+            }
+
+            @Override
+            public void onNext(@NonNull List<String> s) {
+                SocketUtil socketUtil=new SocketUtil( s,1985,SplashActivity.this);
+                //调取方法开始连接
+                socketUtil.getSocketConection();
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+            }
+
+            @Override
+            public void onComplete() {
                 startActivity(new Intent(SplashActivity.this, SelectLinesActivity.class));
                 finish();
             }
-        }, 1500);
+        };
+
+
+        oble.subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(oser);
     }
 }
