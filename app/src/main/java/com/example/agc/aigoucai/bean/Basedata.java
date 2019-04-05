8bc23c9e9f61dc103d7f8a8c6492ed836c95f1f0
DESKTOP-VFHMO2M\o <jason19990099@gmail.com>
2019-04-05 14:41:38 +0800

    爱购彩、

--- app/src/main/java/com/example/agc/aigoucai/bean/Basedata.java
+++ app/src/main/java/com/example/agc/aigoucai/bean/Basedata.java
@@ -3,7 +3,7 @@ package com.example.agc.aigoucai.bean;
 public class Basedata {
     public static  String appid = "agc9001";
     public static  boolean ifgetService = true;
-//    public static final String share_url = "https://www.agcapp.me/app.html";
+    public static final String share_url = "https://www.agcapp.me/app.html";
     //注意要修改配置文件的推送設置
     //爱购彩代理lzx168
 //    public static final String share_url = "https://www.agcapp.me/lzx168.html";
@@ -12,5 +12,5 @@ public class Basedata {
 //    public static final String share_url = "https://www.agcapp.me/agcdaili.html";
 
     //       APPID agc9001
-    public static final String share_url = "https://appv2.com/20190404.html";
+//    public static final String share_url = "https://appv2.com/20190404.html";
 }
