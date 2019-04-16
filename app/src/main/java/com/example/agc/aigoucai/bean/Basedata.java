7b75e8bb3bd5e3ebfca8b4c149a493e92626d0a2
DESKTOP-VFHMO2M\o <jason19990099@gmail.com>
2019-04-16 16:46:22 +0800

    111

--- app/src/main/java/com/example/agc/aigoucai/bean/Basedata.java
+++ app/src/main/java/com/example/agc/aigoucai/bean/Basedata.java
@@ -1,7 +1,7 @@
 package com.example.agc.aigoucai.bean;
 
 public class Basedata {
-    public static  String appid = "agc9001";
+    public static  String appid = "android101";
     public static  boolean ifgetService = true;
     public static final String share_url = "https://www.agcapp.me/app.html";
     //注意要修改配置文件的推送設置
