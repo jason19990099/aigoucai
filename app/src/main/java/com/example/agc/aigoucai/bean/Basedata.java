2308636cb44f6c10e804cecc5b06ebb1adde7796
DESKTOP-VFHMO2M\o <jason19990099@gmail.com>
2019-05-02 15:04:53 +0800

    新包，推送和爱购彩放在一起 。

--- app/src/main/java/com/example/agc/aigoucai/bean/Basedata.java
+++ app/src/main/java/com/example/agc/aigoucai/bean/Basedata.java
@@ -1,9 +1,9 @@
 package com.example.agc.aigoucai.bean;
 
 public class Basedata {
-    public static  String appid = "wgc9001";
+    public static  String appid = "wgc";
     public static  boolean ifgetService = true;
-    public static final String share_url = "https://appkings.me/20190404.html";
+    public static final String share_url = "http://www.appkings.me/wgc.html";
     //注意要修改配置文件的推送設置
 
 }
