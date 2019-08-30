13cc112fa10ec207f1b8dfc4f8db559d79ae5029
DESKTOP-VFHMO2M\o <jason19990099@gmail.com>
2019-08-30 15:57:51 +0800

    1111

--- app/src/main/java/com/example/agc/aigoucai/bean/Basedata.java
+++ app/src/main/java/com/example/agc/aigoucai/bean/Basedata.java
@@ -1,11 +1,14 @@
 package com.example.agc.aigoucai.bean;
 
 public class Basedata {
-    public static final String appid = "android907";
+    public static final String appid = "egvip";
     //    public static final String appid = "android908";
     public static  boolean ifgetService = true;
-    public static final String share_url = "https://www.appkings.me/eg.html";
+//    public static final String share_url = "https://www.appkings.me/eg.html";
 //    public static final String share_url = "https://www.appkings.me/eg2.html";
+    //vip用的链接
+    public static final String share_url = "https://appkings.me/egvip.html";
+
     //注意要修改配置文件的推送設置
 
 }
